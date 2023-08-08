dbee1f68676b26107b0cc9b8e80b7ac33ce3e66e
Paras Jain <Paras.Jain@eclerx.com>
2023-08-08 10:17:51 +0530

    Prediction & UI

---
+++ src/CNN_Classifier/pipeline/prediction.py
@@ -0,0 +1,21 @@
+import os
+import numpy as np
+import tensorflow as tf
+
+
+class PredictionPipeline:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def predict(self):
+        model = tf.keras.models.load_model(os.path.join("artifacts", "training", "model.h5"))
+        imagename = self.filename
+        test_image = tf.keras.preprocessing.image.load_img(imagename, target_size=(224, 224))
+        test_image = tf.keras.preprocessing.image.img_to_array(test_image)
+        test_image = np.expand_dims(test_image, axis=0)
+        result = np.argmax(model.predict(test_image), axis=1)
+
+        if result[0] == 1:
+            return [{"image": 'Healthy'}]
+        else:
+            return [{"image": 'Coccidiosis'}]
